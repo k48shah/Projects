1f18cdcf014c0c2f4ed4b3d0f23f656c4bbc1e5f
k48shah <48638232+k48shah@users.noreply.github.com>
2019-03-17 01:51:21 -0400

    Initial commit

---
+++ Unity Visual Neural Network/README.txt.txt
@@ -0,0 +1,6 @@
+Unfortunately The code is all I have saved of the project. The unity project prefabs and scenes were deleted during a change of hardware. 
+
+Think of the project like a collision avoidance system for a car, except the car learns to avoid obstacles by crashing for a given number of generations.
+
+There needs to be a track, and a vehicle. 'Checkpoints' are invisible planes with triggers set up to find the "best fit" in a generation. 
+(Yes there are genetic algorithms involved as well)
---
+++ Unity Visual Neural Network/hit.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hit : MonoBehaviour
+{
+    public int checkpoints;
+    public Material Passed;
+    public Vector3 init_pos;
+    public Quaternion init_rotation;
+    public bool crash;
+
+    Agent agent;
+    // Use this for initialization
+    void Start()
+    {
+        agent = gameObject.GetComponent<Agent>();
+        crash = false;
+        checkpoints = 0;
+        init_pos = transform.position;
+        init_rotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Checkpoint")
+        {
+            Renderer tmp = other.gameObject.GetComponent<Renderer>();
+            Checkpoint t = other.gameObject.GetComponent<Checkpoint>();
+            bool p = t.passed;
+            if (!p)
+            {
+                t.SetBool(true);
+                tmp.material = Passed;
+                checkpoints++;
+                agent.dist += 1.0f;
+            }
+        }
+        else
+        {
+            //hit a wall considered fail
+            //checkpoints = 0;
+            crash = true;
+        }
+    }
+}
